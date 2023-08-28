026d22db12e7b83cb6af6ebd2b206630ac26b37a
mokhelek <mkatleho191@gmail.com>
2023-08-28 13:14:15 +0200

    moved auth logic to services folder

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,67 +2,22 @@ import express from "express";
 var router = express.Router();
 
 import db from "../model/db.js";
-import bcrypt from 'bcrypt';
+import authService from "../services/authService.js";
+
+const authServiceInstance = authService(db);
+
+
 
 router.get("/login", (req, res) => {
     res.render("login");
 });
 
-router.post("/login", async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const waiter = await db.oneOrNone("SELECT * FROM waiters WHERE username = $1", username);
-        const admin = await db.oneOrNone("SELECT * FROM admin WHERE username = $1", username);
-
-        if (waiter) {
-            const passwordMatch = await bcrypt.compare(password, waiter.password);
-
-            if (passwordMatch) {
-                req.session.user = waiter;
-                res.redirect(`/waiters/${username}`);
-            } else {
-                res.redirect("/auth/login");
-            }
-        } else if (admin) {
-            const passwordMatch = await bcrypt.compare(password, admin.password);
-            if (passwordMatch) {
-                req.session.user = admin;
-                res.redirect(`/admin/${username}`);
-            } else {
-                res.redirect("/auth/login");
-            }
-        } else {
-            res.redirect("/auth/login");
-        }
-    } catch (error) {
-        res.redirect("/auth/login");
-        console.log("I HAVE AN ERROR ****");
-    }
-});
-
-
 router.get("/register", (req, res) => {
     res.render("register");
 });
 
-router.post("/register", async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        await db.none("INSERT INTO waiters(username, password) VALUES($1, $2)", [username, hashedPassword]);
-        res.redirect("/login/user");
-    } catch (error) {
-        console.log(error);
-        res.redirect("/register");
-    }
-});
-
-router.get("/logout", (req, res) => {
-    req.session.destroy(() => {
-        res.redirect("/login/user");
-    });
-});
+router.post("/login",authServiceInstance.userLogin );
+router.post("/register", authServiceInstance.userRegistration );
+router.get("/logout", authServiceInstance.userLogout );
 
 export default router ;
---
+++ services/authService.js
@@ -0,0 +1,57 @@
+import bcrypt from "bcrypt";
+
+export default function authService(db) {
+    async function userLogin(req, res) {
+        const { username, password } = req.body;
+
+        const waiter = await db.oneOrNone("SELECT * FROM waiters WHERE username = $1", username);
+        const admin = await db.oneOrNone("SELECT * FROM admin WHERE username = $1", username);
+
+        if (waiter) {
+            const passwordMatch = await bcrypt.compare(password, waiter.password);
+
+            if (passwordMatch) {
+                req.session.user = waiter;
+                res.redirect(`/waiters/${username}`);
+            } else {
+                res.redirect("/auth/login");
+            }
+        } else if (admin) {
+            const passwordMatch = await bcrypt.compare(password, admin.password);
+            if (passwordMatch) {
+                req.session.user = admin;
+                res.redirect(`/admin/${username}`);
+            } else {
+                res.redirect("/auth/login");
+            }
+        } else {
+            res.redirect("/auth/login");
+        }
+    }
+
+    async function userRegistration(req, res) {
+        const { username, password } = req.body;
+
+        try {
+            const hashedPassword = await bcrypt.hash(password, 10);
+            await db.none("INSERT INTO waiters(username, password) VALUES($1, $2)", [username, hashedPassword]);
+            res.redirect("/login/user");
+        } catch (error) {
+            console.log(error);
+            res.redirect("/register");
+        }
+    }
+
+
+    async function userLogout(req, res){
+        req.session.destroy(() => {
+            res.redirect("/auth/login");
+        });
+    }
+
+    return {
+        userLogin,
+        userRegistration,
+        userLogout
+    };
+}
