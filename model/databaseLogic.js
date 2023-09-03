a04ffefe3800d42b51981224419fb03442ef0957
mokhelek <mkatleho191@gmail.com>
2023-09-03 22:39:06 +0200

    add database tests

--- model/databaseLogic.js
+++ model/databaseLogic.js
@@ -1,3 +1,46 @@
 export default function databaseInteraction(db) {
-    return {};
+    async function addWaiter(username, password) {
+        await db.none("INSERT INTO waiters(username, password) VALUES ($1, $2)", [username, password]);
+    }
+    async function allWaiters() {
+        return await db.any("SELECT * FROM waiters");
+    }
+
+    async function allDays() {
+        return await db.any("SELECT * FROM all_days");
+    }
+
+    async function addDay() {
+        const selectedDays = ['Monday'];
+        let authenticatedUser = 'Kat' ;
+
+        const insertQueries = selectedDays.map(async (item) => {
+            const insertQuery = `
+                    INSERT INTO days (username, weekday)
+                    VALUES ($1, $2)
+                    ON CONFLICT (username, weekday) DO NOTHING
+                    RETURNING id, username, weekday;
+                `;
+            let result = await db.oneOrNone(insertQuery, [authenticatedUser, item]);
+
+            if (result != null) {
+                const insertQueriesCounter = selectedDays.map(async (item) => {
+                    return await db.none("UPDATE all_days SET counter = all_days.counter + 1, status = CASE WHEN all_days.counter < 2  THEN 'insufficient' WHEN all_days.counter > 2 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [item]);
+                });
+
+                await Promise.all(insertQueriesCounter);
+            } else {
+                // TODO : --> Ask if user want to update the day ?
+                console.log("WOULD YOU LIKE TO UPDATE AVAILABILITY ??");
+            }
+            return result;
+        });
+        await Promise.all(insertQueries);
+    }
+
+    return {
+        addWaiter,
+        allWaiters,
+        allDays,
+    };
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import assert from "assert";
 import pgPromise from "pg-promise";
-import 'dotenv/config';
+import "dotenv/config";
 
 import databaseInteraction from "../model/databaseLogic.js";
 
@@ -8,7 +8,6 @@ const connectionTest = process.env.DATABASE_URL_TEST;
 
 const db = pgPromise()(connectionTest);
 db.connect();
-
 
 const databaseInstance = databaseInteraction(db);
 
@@ -19,15 +18,23 @@ describe("The basic database web app", function () {
         await db.none("DELETE FROM days");
         await db.none("UPDATE all_days SET counter = 0");
         await db.none("UPDATE all_days SET counter = 0, status = 'insufficient' ");
+        await db.none("DELETE FROM waiters");
     });
 
     it("Should add a waiter", async function () {
-        // await databaseInstance.addRegistration(db, "CA258") ;
-
-        // let regNums = await databaseInstance.getRegistrations(db,"ALL") ;
-
-        assert.equal(1, 1);
+        await databaseInstance.addWaiter("Kat", "1234");
+        let allWaiters = await databaseInstance.allWaiters();
+        assert.equal(1, allWaiters.length);
     });
-
-
-})
+    it("Should add a multiple waiters", async function () {
+        await databaseInstance.addWaiter("Kat", "1234");
+        await databaseInstance.addWaiter("Mpho", "1234");
+        await databaseInstance.addWaiter("Tom", "1234");
+        let allWaiters = await databaseInstance.allWaiters();
+        assert.equal(3, allWaiters.length);
+    });
+    it("It should return data for all days", async function () {
+        let allDays = await databaseInstance.allDays();
+        assert.equal(7, allDays.length);
+    });
+});
