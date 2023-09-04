619c5f55e48ef25f9deb209f9a832fe21a7deae6
mokhelek <mkatleho191@gmail.com>
2023-09-05 01:49:57 +0200

    separated waiter logic from routes

--- routes/waiter.js
+++ routes/waiter.js
@@ -2,6 +2,10 @@ import express from "express";
 var router = express.Router();
 
 import db from "../model/db.js";
+import waiterService from "../services/waiterService.js";
+
+
+let waiterServiceInstance = waiterService(db);
 
 function isAuthenticated(req, res, next) {
     if (req.session.user) {
@@ -10,61 +14,10 @@ function isAuthenticated(req, res, next) {
     res.redirect("/auth/login");
 }
 
-router.get("/:username", isAuthenticated, async (req, res) => {
-    // todo: -> Days booked by the current logged-in waiter.
-    let days = await db.any("SELECT * FROM days WHERE username = $1", [req.session.user.username]);
-    let all_days = await db.any("SELECT * FROM all_days ORDER BY id ASC");
-    let authenticatedUser = req.session.user.username;
-    res.render("home", { days, all_days, authenticatedUser });
-});
-
-router.post("/:username", async (req, res) => {
-    const selectedValues = JSON.parse(req.body.body);
-    let authenticatedUser = req.session.user.username;
-
-    const insertQuery = `
-    INSERT INTO days (username, weekday)
-    VALUES ($1, $2)
-    ON CONFLICT (username, weekday) DO NOTHING
-    RETURNING id, username, weekday;
-`;
-
-    let validDays = []
-    for (let i of selectedValues) {
-        let result = await db.oneOrNone(insertQuery, [authenticatedUser, i]);
-        validDays.push(result) 
-    }
-
-    for (let i of validDays) {
-        if(i != null){
-           await db.none("UPDATE all_days SET counter = all_days.counter + 1, status = CASE WHEN all_days.counter < 2  THEN 'insufficient' WHEN all_days.counter > 2 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [i.weekday]);
-        }
-    }
-    
-    res.redirect("/");
-});
-
-router.get("/remove/:id", isAuthenticated, async (req, res) => {
-    let day = await db.oneOrNone("SELECT weekday from days WHERE id = $1", req.params.id);
-    await db.none("DELETE FROM days WHERE id = $1", req.params.id);
-    await db.none("UPDATE all_days SET counter = all_days.counter - 1, status = CASE WHEN all_days.counter < 2  THEN 'insufficient' WHEN all_days.counter > 2 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [day.weekday]);
-    await db.none("UPDATE all_days SET status = CASE WHEN all_days.counter < 3  THEN 'insufficient' WHEN all_days.counter > 3 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [day.weekday]);
-
-    res.redirect("/");
-});
+router.get("/:username", isAuthenticated, waiterServiceInstance.waiterHome );
+router.post("/:username",waiterServiceInstance.addDays);
+router.get("/remove/:id", isAuthenticated, waiterServiceInstance.updateDays);
 
 export default router;
 
-/*
 
-        if (result != null) {
-                const insertQueriesCounter = selectedValues.map(async (item) => {
-                    return await db.none("UPDATE all_days SET counter = all_days.counter + 1, status = CASE WHEN all_days.counter < 2  THEN 'insufficient' WHEN all_days.counter > 2 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [item]);
-                });
-
-                await Promise.all(insertQueriesCounter);
-            } else {
-                console.log("WOULD YOU LIKE TO UPDATE AVAILABILITY ??");
-            }
-
-            */
---
+++ services/waiterService.js
@@ -0,0 +1,49 @@
+export default function waiterService(db) {
+    async function waiterHome(req, res) {
+        let days = await db.any("SELECT * FROM days WHERE username = $1", [req.session.user.username]);
+        let all_days = await db.any("SELECT * FROM all_days ORDER BY id ASC");
+        let authenticatedUser = req.session.user.username;
+        res.render("home", { days, all_days, authenticatedUser });
+    }
+
+    async function addDays(req, res) {
+        const selectedValues = JSON.parse(req.body.body);
+        let authenticatedUser = req.session.user.username;
+
+        const insertQuery = `
+        INSERT INTO days (username, weekday)
+        VALUES ($1, $2)
+        ON CONFLICT (username, weekday) DO NOTHING
+        RETURNING id, username, weekday;
+    `;
+
+        let validDays = [];
+        for (let i of selectedValues) {
+            let result = await db.oneOrNone(insertQuery, [authenticatedUser, i]);
+            validDays.push(result);
+        }
+
+        for (let i of validDays) {
+            if (i != null) {
+                await db.none("UPDATE all_days SET counter = all_days.counter + 1, status = CASE WHEN all_days.counter < 2  THEN 'insufficient' WHEN all_days.counter > 2 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [i.weekday]);
+            }
+        }
+
+        res.redirect("/");
+    }
+
+    async function updateDays(req,res){
+        let day = await db.oneOrNone("SELECT weekday from days WHERE id = $1", req.params.id);
+        await db.none("DELETE FROM days WHERE id = $1", req.params.id);
+        await db.none("UPDATE all_days SET counter = all_days.counter - 1, status = CASE WHEN all_days.counter < 2  THEN 'insufficient' WHEN all_days.counter > 2 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [day.weekday]);
+        await db.none("UPDATE all_days SET status = CASE WHEN all_days.counter < 3  THEN 'insufficient' WHEN all_days.counter > 3 THEN 'surplus' ELSE 'sufficient' END WHERE weekday = $1", [day.weekday]);
+    
+        res.redirect("/");
+    }
+
+    return {
+        waiterHome,
+        addDays,
+        updateDays
+    };
+}
